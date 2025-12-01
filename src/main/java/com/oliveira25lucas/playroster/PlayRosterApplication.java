3868bef33ceadd5252ba0023e517423c057f3fbe
oliveira25lucas <lucas1122oliveira@gmail.com>
2025-12-01 18:12:29 -0300

    feat: add Spring Init

---
+++ src/main/java/com/oliveira25lucas/playroster/PlayRosterApplication.java
@@ -0,0 +1,13 @@
+package com.oliveira25lucas.playroster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayRosterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayRosterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oliveira25lucas/playroster/PlayRosterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oliveira25lucas.playroster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayRosterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
